bc07d3fdb2eeea533303005f713dabb6d5cd06e4
Dominik Serwatka <263915@student.pwr.edu.pl>
2023-11-26 18:34:01 +0100

    Add reeborg code to main.py

--- Day6/main.py
+++ Day6/main.py
@@ -1,1 +1,18 @@
 ## code for reeborg 
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while not at_goal():
+    
+#     while wall_on_right():
+#         if not wall_in_front():
+#             move()
+#         else:
+#             turn_left()
+#     if not wall_on_right() and not wall_in_front():
+#         move()
+#     turn_right()
+#     if not wall_in_front() and not at_goal():
+#         move()
