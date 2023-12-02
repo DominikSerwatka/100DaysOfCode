a97612c65e31aa66977b6600964201b0b8a1433b
Dominik Serwatka <263915@student.pwr.edu.pl>
2023-12-02 22:39:16 +0100

    Add main.py Cofffe maker machine OOP

---
+++ Day16/main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+is_run = True
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+while is_run:
+    user_input = input(f"What would you like? ({menu.get_items()}): ")
+    if user_input == "off":
+        is_run = False
+    elif user_input == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
