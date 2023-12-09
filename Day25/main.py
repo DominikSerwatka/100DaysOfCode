9ec2f30c09249196adf92ae31fb16e2a81c8da07
Dominik Serwatka <263915@student.pwr.edu.pl>
2023-12-09 02:16:22 +0100

    Add working with csv file, guess state game

--- Day25/main.py
+++ Day25/main.py
@@ -1,37 +1,17 @@
-# data_list = []
-# with open("weather_data.csv", "r") as file:
-#     data_list.append(file.readlines())
-#
-# for item in data_list:
-#     print(item)
-#
-# import csv
-#
-# with open("weather_data.csv", mode="r") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         print(row)
-#         if row[1] != 'temp':
-#             temperatures.append(int(row[1]))
-# print(temperatures)
-
 import pandas
-
-data = pandas.read_csv("weather_data.csv")
-# print(data)
-print(type(data))
-print(type(data["temp"]))
-data_dict = data.to_dict()
-print(data_dict['day'][0])
-temp_list = data["temp"].to_list()
-print(temp_list)
-summary = 0
-for item in temp_list:
-    summary += item
-print(f"Avg temperature: {round(summary/len(temp_list))}")
-average = sum(temp_list) / len(temp_list)
-print(average)
-#
-# avg = sum_temp/len(temp_list)
-# print(avg)
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+count_gray = data['Primary Fur Color'].value_counts()["Gray"]
+count_black = data['Primary Fur Color'].value_counts()["Black"]
+count_cinnamon = data['Primary Fur Color'].value_counts()["Cinnamon"]
+count = data['Primary Fur Color'].value_counts()
+print(count_gray)
+print(count_black)
+print(count_cinnamon)
+print(count)
+data_fur = {
+    "Fur" : ["grey", "red", "black"],
+    "Count" : [count_gray, count_cinnamon, count_black],
+}
+data2 = pandas.DataFrame(data_fur)
+data2.to_csv("squirrel_count.csv")
+print(data2)
